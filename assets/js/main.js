2866085db57c9a308e1f7b6d759e46bd923577d5
amirreza <amirreza.firoz13@gmail.com>
2024-08-22 11:46:50 +0330

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,7 +16,7 @@ x.forEach(e => {
 });
 
 var aboutButton = document.querySelector('.footer-dvivition:nth-child(2)');
-x[0].addEventListener('click', e => {
+x[1].addEventListener('click', e => {
     document.querySelector('body>#aboutUS').style.display = !displayAboutUs && !displaySupportMenu ? 'flex' : 'none';
     displayAboutUs = !displaySupportMenu ? !displayAboutUs : displayAboutUs;
     aboutButton.style.animation = 'slide-up-reverse 0.3s ease';
@@ -38,18 +38,3 @@ document.querySelector('#support .box .close').addEventListener('click', e => {
     document.querySelector('body>#support').style.display = !displaySupportMenu && !displaySupportMenu ? 'flex' : 'none';
     displaySupportMenu = !displayAboutUs ? !displaySupportMenu : displaySupportMenu;
 });
-
-var list = document.querySelector('section .list ul');
-fetch('methods.json').then((resp) => resp.text()).then(function (data) {
-    let parse = JSON.parse(data);
-    parse.forEach(e => {
-        let newATag = document.createElement('a');
-        let newLiTag = document.createElement('li');
-        newLiTag.append(document.createTextNode(e));
-        newATag.appendChild(newLiTag);
-        newATag.setAttribute('href', `${e}.html`);
-        list.appendChild(newATag);
-    });
-});
-
-document.querySelector('.buttons .button');
