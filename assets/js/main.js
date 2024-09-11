7b4de4b05f122411e44301e5051f2f952adcd92a
amirreza <amirreza.firoz13@gmail.com>
2024-09-11 13:30:34 +0330

    going back

---
+++ assets/js/main.js
@@ -0,0 +1,40 @@
+let x = document.querySelectorAll('.footer-dvivition');
+let displayAboutUs = false;
+let displaySupportMenu = false;
+
+x.forEach(e => {
+    e.addEventListener('mouseover', b => {
+        if (displayAboutUs || displaySupportMenu) return
+        e.style.animation = 'slide-up 0.3s ease';
+        e.style.transform = 'translateY(-10px)';
+    });
+    e.addEventListener('mouseout', b => {
+        if (displayAboutUs || displaySupportMenu) return
+        e.style.animation = 'slide-up-reverse 0.3s ease';
+        e.style.transform = '';
+    });
+});
+
+var aboutButton = document.querySelector('.footer-dvivition:nth-child(2)');
+x[1].addEventListener('click', e => {
+    document.querySelector('body>#aboutUS').style.display = !displayAboutUs && !displaySupportMenu ? 'flex' : 'none';
+    displayAboutUs = !displaySupportMenu ? !displayAboutUs : displayAboutUs;
+    aboutButton.style.animation = 'slide-up-reverse 0.3s ease';
+    aboutButton.style.transform = '';
+});
+document.querySelector('#aboutUS .box .close').addEventListener('click', e => {
+    document.querySelector('body>#aboutUS').style.display = !displayAboutUs && !displaySupportMenu ? 'flex' : 'none';
+    displayAboutUs = !displaySupportMenu ? !displayAboutUs : displayAboutUs;
+});
+
+var supportButton = document.querySelector('.footer-dvivition:last-child');
+x[x.length - 1].addEventListener('click', e => {
+    document.querySelector('body>#support').style.display = !displaySupportMenu && !displayAboutUs ? 'flex' : 'none';
+    displaySupportMenu = !displayAboutUs ? !displaySupportMenu : displaySupportMenu;
+    supportButton.style.animation = 'slide-up-reverse 0.3s ease';
+    supportButton.style.transform = '';
+});
+document.querySelector('#support .box .close').addEventListener('click', e => {
+    document.querySelector('body>#support').style.display = !displaySupportMenu && !displaySupportMenu ? 'flex' : 'none';
+    displaySupportMenu = !displayAboutUs ? !displaySupportMenu : displaySupportMenu;
+});
