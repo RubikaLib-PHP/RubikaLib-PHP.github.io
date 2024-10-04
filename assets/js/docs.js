70ce6cdf95ce325feb23b2c0726016411eee1bca
amirreza <amirreza.firoz13@gmail.com>
2024-10-04 20:41:00 +0330

    some debugs

--- assets/js/docs.js
+++ assets/js/docs.js
@@ -3,16 +3,14 @@ document.querySelectorAll('header nav a').forEach(el => {
         if (window.innerWidth < 630) return;
         el.style.animation = "MakeHeaderNavShadow2 0.15s linear";
         el.style.boxShadow = "0 2px 5px #1b1b40";
-        el.style.height = '38px';
-        el.style.width = '68px';
+        el.style.width = '88px';
         el.style.marginRight = '7px';
     })
     el.addEventListener('mouseout', ev => {
         if (window.innerWidth < 630) return;
         el.style.animation = "RemoveHeaderNavShadow2 0.15s linear"
         el.style.boxShadow = "none";
-        el.style.height = '40px';
-        el.style.width = '70px';
+        el.style.width = '90px';
         el.style.marginRight = '5px';
     })
 });
@@ -21,10 +19,19 @@ let p = document.querySelector('aside nav > div');
 p.addEventListener('mouseover', ev => {
     p.style.animation = "DocsNavCloseBTN-Up 0.15s linear";
     p.style.boxShadow = "0 2px 5px #1b1b40";
-    // p.style.marginRight = '1px';
 });
 p.addEventListener('mouseout', ev => {
     p.style.animation = "DocsNavCloseBTN-Down 0.15s linear";
     p.style.boxShadow = "none";
-    // p.style.marginRight = '0';
+});
+
+let open_nav_btn = document.querySelector('body > .open_nav_btn');
+open_nav_btn.addEventListener('click', () => {
+    open_nav_btn.style.display = 'none';
+    document.querySelector('body main aside').style.display = 'flex';
+});
+let aside_cls_btn = document.querySelector('body main aside .cls');
+aside_cls_btn.addEventListener('click', () => {
+    open_nav_btn.style.display = 'flex';
+    document.querySelector('body main aside').style.display = 'none';
 });
