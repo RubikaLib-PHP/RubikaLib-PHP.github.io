e4452bd36806a498502b596995ff25ae70287a78
amirreza <amirreza.firoz13@gmail.com>
2024-10-08 13:52:22 +0330

    minify everythink

---
+++ assets/js/home-min.js
@@ -0,0 +1,1 @@
+let b=document.querySelector("#a1 h3");b.addEventListener("mouseover",(e=>{b.style.animation="MakeTopARCShadow 0.15s linear",b.style.boxShadow="0 5px 10px #eaeaea"})),b.addEventListener("mouseout",(e=>{b.style.animation="RemoveTopARCShadow 0.15s linear",b.style.boxShadow="none"}));
